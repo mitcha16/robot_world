ac60e1f9fe1d4c4f12a11edef77084cbfcb0a2ba
mitchashby16 <mitchashby16@gmail.com>
2015-07-07 13:25:34 -0600

    initial commit

---
+++ app/controllers/robot_world.rb
@@ -0,0 +1,11 @@
+require 'models/robot_world'
+
+class RobotWorldApp < Sinatra::Base
+  set :root, File.join(File.dirname(__FILE__), '..')
+  set :method_override, true
+
+  get "/" do
+    erb :dashboard
+  end
+
+end
